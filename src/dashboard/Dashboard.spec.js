7343efb0c15ac6d58ef244c992e1d918b7360240
Jesse-Tingle <jtingle0@gmail.com>
2019-11-02 02:58:06 -0400

    added test to check that Dashboard renders without crashing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+ 
+import ReactDOM from 'react-dom'
+
+// import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+it('Renders dashboard without crashing', () => {
+    const div = document.createElement('div');
+    
+    // const element = wrapper.getByText(/text here/i);
+    ReactDOM.render(<Dashboard />, div)    
+})
