e05112d3016eb8e5294f79743bdb557894eb0232
Jesse-Tingle <jtingle0@gmail.com>
2019-11-02 03:46:25 -0400

    add test to check for default state of dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,13 +4,24 @@ import Dashboard from './Dashboard';
  
 import ReactDOM from 'react-dom'
 
-// import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+import * as rtl from '@testing-library/react'; 
+
+afterEach(rtl.cleanup);
 
 it('Renders dashboard without crashing', () => {
     const div = document.createElement('div');
     
-    // const element = wrapper.getByText(/text here/i);
     ReactDOM.render(<Dashboard />, div)    
+})
+
+
+it('Default state of Unlocked, Open, Lock Gate, Close Gate', () => {
+    const { getByText } = rtl.render(<Dashboard />);
+
+    expect(getByText('Unlocked'));
+    expect(getByText('Open'));
+    expect(getByText('Lock Gate'));
+    expect(getByText('Close Gate'));
 })
